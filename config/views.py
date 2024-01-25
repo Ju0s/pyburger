e9837cadf1aef8864eb0a7a045291e290b716383
juyoung <juyoungeeya@gmail.com>
2024-01-25 16:44:00 +0900

    project finish

--- config/views.py
+++ config/views.py
@@ -4,7 +4,8 @@
 
 # from django.http import HttpResponse
 from django.shortcuts import render
-
+from burgers.models import Burger
+import pandas as pd
 # def main(request):
 #     return HttpResponse("안녕하세요, pyburger입니다")
 
@@ -12,4 +13,32 @@ def main(request):
     return render(request, "main.html")
 
 def burger_list(request):
-    return render(request, "burger_list.html")
+    # burgers = Burger.objects.all()
+    burgers = Burger.objects.all().values()
+    data = pd.DataFrame(burgers)
+    print(data)
+    # print("전체 햄버거 목록:", burgers)
+
+    context = {
+        # "burgers": burgers,
+        "burgers": data.to_html(classes="table table-dark table-striped", index=False, justify="match-parent")
+    }
+
+    return render(request, "burger_list.html", context)
+
+def burger_search(request):
+    keyword = request.GET.get("keyword")
+    # print(keyword)
+
+    if keyword is not None:
+        burgers = Burger.objects.filter(name__contains=keyword)
+
+    else:
+        burgers = Burger.objects.none()
+
+    # print(burgers)
+    context = {
+        "burgers": burgers,
+    }
+
+    return render(request, "burger_search.html", context)
